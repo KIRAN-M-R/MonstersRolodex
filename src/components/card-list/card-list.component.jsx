ca4ea92273fcbe00cc6e831def78418344d1044e
KIRAN M R <iamthekiranmr@gmail.com>
2023-01-22 20:25:48 +0530

    UI styled to cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,22 @@
 import { Component } from "react";
+import './card-list.styles.css';
 
 class CardList extends Component {
     render(){
         console.log(this.props)
         const {monsters} = this.props;
         return (
-        <div>
+        <div className="card-list">
             {
-                monsters.map((monster)=>(
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))
+                monsters.map((monster)=>{
+                    const {name, email, id} = monster;
+                    return (
+                    <div className="card-container" key={id} >
+                        <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                )})
             }
         </div>
         )
