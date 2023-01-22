c1ed82e24f05c7a332b6997109a9a95925bcc518
KIRAN M R <iamthekiranmr@gmail.com>
2023-01-22 11:38:34 +0530

    added api call

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState(()=>{
+        return {monsters: users}
+      }, ()=>{console.log(this.state.monsters)}));
+  }
+  render() {
+    console.log('render');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
-  
 }
 
 export default App;
