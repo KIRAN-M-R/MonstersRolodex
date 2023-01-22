cc0f75985c392df0a66e86c949fcd1d43771488f
KIRAN M R <iamthekiranmr@gmail.com>
2023-01-22 21:23:54 +0530

    monsters roledex with class components finished

--- src/App.js
+++ src/App.js
@@ -10,25 +10,19 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-
-    
   }
 
   componentDidMount() {
-    
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
   onSearchChange = (event) => {
-    
     const searchField = event.target.value.toLocaleLowerCase();
 
     this.setState(() => {
@@ -36,8 +30,6 @@ class App extends Component {
     });
   };
   render() {
-    
-
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
@@ -46,7 +38,12 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <SearchBox onChangeHandler={onSearchChange} placeholder='search monsters' className='monsters-search-box' />
+        <h1 className="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="search monsters"
+          className="monsters-search-box"
+        />
         {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
@@ -54,7 +51,7 @@ class App extends Component {
             </div>
           );
         })} */}
-        <CardList monsters = {filteredMonsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import './card-list.styles.css';
+import Card from "../card/card.component";
 
 class CardList extends Component {
     render(){
@@ -9,13 +10,9 @@ class CardList extends Component {
         <div className="card-list">
             {
                 monsters.map((monster)=>{
-                    const {name, email, id} = monster;
+                    
                     return (
-                    <div className="card-container" key={id} >
-                        <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
+                    <Card monster={monster} />
                 )})
             }
         </div>
